306d384c90c924496415ed39300a8b203bee0e22
Marcelo <verissimomarcelo95@gmail.com>
2025-09-29 20:44:10 -0300

    termino de nav e home

--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -6,7 +6,7 @@ function Inicio() {
     <section id='inicio' className='inicio'>
         <div className='slogan-text'>
           <h1>O Lucro é o combustível da liberdade.</h1>
-          <p>Mentoria financeira para lojistas PJ com <strong className='p1'>Nathália Verissimo</strong></p>
+          <p>Mentoria financeira para lojistas PJ com <strong className='p1'>Nathália Verissimo</strong>.</p>
            <div className='b1'>
             <button onClick={() => {
             document.getElementById("contato").scrollIntoView({ behavior: "smooth" });
