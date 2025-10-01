e93c5563859ea88edbeec76c970af67e78c8c34f
Marcelo <verissimomarcelo95@gmail.com>
2025-10-01 19:51:34 -0300

    começando seção depoimentos

--- src/components/Depoimentos.jsx
+++ src/components/Depoimentos.jsx
@@ -1,16 +1,54 @@
-import React from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import { Navigation, Pagination, Autoplay } from 'swiper/modules';
+import './Depoimentos.css';
 
-function Depoimentos() {
+export default function Depoimentos() {
+  const depoimentos = [
+    {
+      id: 1,
+      name: 'Marcelo Verissimo',
+      loja: 'Hortifruti Irajá',
+      texto: "Com a mentoria Lucro Claro consegui organizar minhas finanças e aumentar meu lucro em 30%.",
+    },
+    {
+      id: 2,
+      name: 'Marcelo Verissimo',
+      loja: 'Hortifruti Irajá',
+      texto: "Com a mentoria Lucro Claro consegui organizar minhas finanças e aumentar meu lucro em 30%.",
+    },
+    { 
+      id: 3,
+      name: 'Marcelo Verissimo',
+      loja: 'Hortifruti Irajá',
+      texto: "Com a mentoria Lucro Claro consegui organizar minhas finanças e aumentar meu lucro em 30%.",
+    },
+  ];
+
   return (
     <section id='depoimentos' className='depoimentos'>
       <h2>Depoimentos</h2>
-      <blockquote>
-        "A mentoria da Nathália foi o ponto de virada para o meu negócio.
-        Hoje, minha loja está organizada e mais lucrativa."
-      </blockquote>
-      <cite>- Marcelo Verissimo, lojista</cite>
+      <Swiper
+        modules={[Navigation, Pagination, Autoplay]}
+        spaceBetween={30}
+        slidesPerView={1}
+        navigation
+        pagination={{ clickable: true}}
+        autoplay= {{ delay: 4000}}
+        loop={true}
+      >
+        {depoimentos.map((dep, index) => (
+          <SwiperSlide key={index}>
+            <div className='depoimento-card'>
+              <p>"{dep.texto}</p>
+              <h4>{dep.nome}</h4>
+              <span>{dep.loja}</span>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </section>
-  )
+  );
 }
-
-export default Depoimentos
