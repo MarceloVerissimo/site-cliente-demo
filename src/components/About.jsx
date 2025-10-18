7722ab371cfe10eb75cee1c1faa416da869b28e5
MarceloVerissimo <verissimomarcelo95@gmail.com>
2025-10-18 13:37:51 -0300

    atualizando css footer, navbar, home e about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,8 +9,8 @@ function About() {
       </div>
 
       <div className='sobre-content'>
-        <h2>Sobre Mim</h2>
-        <p>Eu sou <strong>Nathalia Veríssimo</strong>, Comecei minha jornada empreendendo na loja da minha família e vivi na pele a dificuldade de lidar com o financeiro sem clareza. Me formei em Administração e descobri minha paixão por finanças empresariais. Percebi que muitos empreendedores passam pelo mesmo: trabalham muito, vendem bem, mas nunca veem o lucro real. Por isso, criei minha mentoria: para ensinar de forma simples e prática como organizar o financeiro e ter lucro de verdade. Hoje curso Contabilidade para me aprofundar ainda mais e continuar ajudando empreendedores a construírem negócios lucrativos e leves.</p>
+        <h2 className='titulo1'>Sobre Mim</h2>
+        <p className='texto1'>Eu sou <strong>Nathalia Veríssimo</strong>, Comecei minha jornada empreendendo na loja da minha família e vivi na pele a dificuldade de lidar com o financeiro sem clareza. Me formei em Administração e descobri minha paixão por finanças empresariais. Percebi que muitos empreendedores passam pelo mesmo: trabalham muito, vendem bem, mas nunca veem o lucro real. Por isso, criei minha mentoria: para ensinar de forma simples e prática como organizar o financeiro e ter lucro de verdade. Hoje curso Contabilidade para me aprofundar ainda mais e continuar ajudando empreendedores a construírem negócios lucrativos e leves.</p>
       </div>
     </section>
   )
--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -5,8 +5,8 @@ function Inicio() {
   return (
     <section id='inicio' className='inicio'>
         <div className='slogan-text'>
-          <h1>O Lucro é o combustível da liberdade.</h1>
-          <p>Mentoria financeira para empreendedores com <strong className='p1'>Nathalia Verissimo</strong>.</p>
+          <h1 className='h1inicio'>O Lucro é o combustível da liberdade.</h1>
+          <p className='pinicio'>Mentoria financeira para empreendedores com <strong className='p1'>Nathalia Verissimo</strong>.</p>
            <div className='b1'>
             <button onClick={() => {
             document.getElementById("contato").scrollIntoView({ behavior: "smooth" });
